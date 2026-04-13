b0afae0b0dec04b4975a65d279d7152dd6ef5389
Anjali387 <anjalirgurav694@gmail.com>
2026-04-13 16:20:54 +0530

    Create Login.jsx

---
+++ frontend/frontend/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import API from '../api/axios';
+
+function Login() {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await API.post('/auth/login', form);
+    localStorage.setItem('token', res.data.token);
+    alert("Login Success");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Email" onChange={e => setForm({...form, email: e.target.value})}/>
+      <input type="password" placeholder="Password" onChange={e => setForm({...form, password: e.target.value})}/>
+      <button>Login</button>
+    </form>
+  );
+}
+
+export default Login;
